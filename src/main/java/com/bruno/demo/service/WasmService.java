c48e65c4fb8de898e8d2ca046c3198c8d17bc26d
bruno_l_medeiros <bruno_l_medeiros@estudante.sesisenai.org.br>
2026-03-12 18:25:14 -0300

    feat: set up a chicory instance manager for a web assembly function

--- src/main/java/com/bruno/demo/service/WasmService.java
+++ src/main/java/com/bruno/demo/service/WasmService.java
@@ -1,11 +1,54 @@
 package com.bruno.demo.service;
 
+import com.dylibso.chicory.runtime.ExportFunction;
+import com.dylibso.chicory.runtime.Instance;
+import com.dylibso.chicory.runtime.Memory;
+import com.dylibso.chicory.wasm.Parser;
+import com.dylibso.chicory.wasm.types.Value;
 import org.springframework.stereotype.Service;
+
+import java.io.File;
 
 @Service
 public class WasmService {
 
-    public byte[] invertImage(byte[] image) {
+    private final Instance instance;
+    private final ExportFunction alloc;
+    private final ExportFunction dealloc;
+    private final ExportFunction invertImageFunc;
 
+    public WasmService() {
+
+        File wasmFile = new File("./image-processor-wasm/target/wasm32-unknown-unknown/release/seu_projeto.wasm");
+
+        this.instance = Instance.builder(Parser.parse(wasmFile)).build();
+        this.alloc = instance.export("alloc");
+        this.dealloc = instance.export("dealloc");
+        this.invertImageFunc = instance.export("invertImage");
+    }
+
+    public byte[] invertImage(byte[] image) {
+        int len = image.length;
+
+        int ptr = allocateAndWrite(image);
+
+        try {
+
+            invertImageFunc.apply(ptr, len);
+
+            Memory memory = instance.memory();
+            return memory.readBytes(ptr,len);
+
+        } finally {
+            dealloc.apply(ptr,len);
+        }
+    }
+
+    private int allocateAndWrite(byte[] image) {
+        int ptrValue = (int) alloc.apply(image.length)[0];
+
+        instance.memory().write(ptrValue, image);
+
+        return ptrValue;
     }
 }
