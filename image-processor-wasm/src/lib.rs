d50329499c30d15a3205b8581ba36f1007f377bb
bruno_l_medeiros <bruno_l_medeiros@estudante.sesisenai.org.br>
2026-03-12 20:55:41 -0300

    feat: create rust logic to invert pixels

--- image-processor-wasm/src/lib.rs
+++ image-processor-wasm/src/lib.rs
@@ -1,14 +1,28 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::mem;
+
+#[unsafe(no_mangle)]
+pub extern "C" fn alloc(capacity: usize) -> *mut u8 {
+    let mut buf = Vec::with_capacity(capacity);
+    let ptr = buf.as_mut_ptr();
+    mem::forget(buf);
+    ptr
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+#[unsafe(no_mangle)]
+pub extern "C" fn dealloc(ptr: *mut u8, capacity: usize) {
+    unsafe {
+        let _ = Vec::from_raw_parts(ptr, 0, capacity);
     }
 }
+
+#[unsafe(no_mangle)]
+pub extern "C" fn invertImage(ptr: *mut u8, len: usize) {
+    unsafe {
+        let slice = std::slice::from_raw_parts_mut(ptr, len);
+
+        for byte in slice.iter_mut() {
+            *byte = 255 - *byte;
+        }
+    }
+}
+
