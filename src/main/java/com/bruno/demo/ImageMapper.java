dc2adef277c49f555cfc46ddc349176e458c7b95
bruno_l_medeiros <bruno_l_medeiros@estudante.sesisenai.org.br>
2026-03-12 18:24:01 -0300

    feat: create a function to get image format

--- src/main/java/com/bruno/demo/ImageMapper.java
+++ src/main/java/com/bruno/demo/ImageMapper.java
@@ -1,6 +1,7 @@
 package com.bruno.demo;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
@@ -10,11 +11,28 @@ import java.io.IOException;
 @Component
 public class ImageMapper {
 
-    ByteArrayOutputStream baos = new ByteArrayOutputStream();
 
     public byte[] toByte(BufferedImage bufferedImage, String type) throws IOException {
-        ImageIO.write(bufferedImage, type, baos);
+        try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
 
-        return baos.toByteArray();
+            boolean success = ImageIO.write(bufferedImage, type, baos);
+
+            if (!success) {
+                ImageIO.write(bufferedImage, "png", baos);
+            }
+
+            return baos.toByteArray();
+        }
+    }
+
+    public String getFormatFromMultipart(MultipartFile file) {
+        String contentType = file.getContentType();
+
+        if (contentType != null && contentType.contains("/")) {
+
+            return contentType.split("/")[1];
+        }
+
+        return "png";
     }
 }
